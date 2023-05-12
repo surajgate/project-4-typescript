2f5535ca90cbc4c657794355aa583d59e9804702
suraj gate <surajgate73@gmail.com>
2023-05-12 10:01:49 +0530

    Material UI changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import data from './data'
+import data from "./data";
 import Question from "./Question";
+import { Container, Paper, Box, Typography, Accordion } from "@mui/material";
 
 interface QuestionType {
   id: number;
@@ -9,19 +10,38 @@ interface QuestionType {
 }
 
 function App() {
-  const [questions, setQuestion] = useState<QuestionType[]>(data );
+  const [questions, setQuestion] = useState<QuestionType[]>(data);
 
   return (
-    <main>
-      <div className="container">
-        <h3>Questions and Answers about Login</h3>
-        <section className="info">
-          {questions.map((question=>{
-            return <Question key = {question.id} {...question}/>
-          }))}
-        </section>
-      </div>
-    </main>);
+    <>
+      <Container>
+        <Paper sx={{display : 'flex', justifyContent : 'center', alignItems : 'center', marginTop : '25vh', p : 1}}>
+          <Container sx={{display : 'flex'}}>
+            <Box>
+              <Typography variant="h4">
+                Questions and Answers about Login
+              </Typography>
+            </Box>
+            <Box>
+              {questions.map((question) => {
+                return <Question key={question.id} {...question} />;
+              })}
+            </Box>
+          </Container>
+        </Paper>
+      </Container>
+    </>
+    // <main>
+    //   <div className="container">
+    //     <h3>Questions and Answers about Login</h3>
+    //     <section className="info">
+    //       {questions.map((question=>{
+    //         return <Question key = {question.id} {...question}/>
+    //       }))}
+    //     </section>
+    //   </div>
+    // </main>
+  );
 }
 
 export default App;
--- src/Question.tsx
+++ src/Question.tsx
@@ -1,32 +1,58 @@
-import { info } from 'console';
-import React, {useState} from 'react'
-import {AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
+import { info } from "console";
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import {
+  Container,
+  Paper,
+  Box,
+  Typography,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from "@mui/material";
 
-interface QuestionProp  {
-    id : number;
-    title : string;
-    info : string
+interface QuestionProp {
+  id: number;
+  title: string;
+  info: string;
 }
 
-const Question = ({title, info}: QuestionProp) => {
+const Question = ({ title, info }: QuestionProp) => {
   const [showInfo, setShowInfo] = useState<boolean>();
 
   return (
-    <article className="question">
-      <header>
-        <h4>{title}</h4>
-        <button
-          className="btn"
-          onClick={() => {
-            setShowInfo(!showInfo);
-          }}
-        >
-          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
-        </button>
-        {showInfo && <p>{info}</p>}
-      </header>
-    </article>
+    <>
+      <Container>
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography variant="h6" gutterBottom>
+              {title}
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography paragraph gutterBottom>
+              {info}
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      </Container>
+    </>
+    // <article className="question">
+    //   <header>
+    //     <h4>{title}</h4>
+    //     <button
+    //       className="btn"
+    //       onClick={() => {
+    //         setShowInfo(!showInfo);
+    //       }}
+    //     >
+    //       {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+    //     </button>
+    //     {showInfo && <p>{info}</p>}
+    //   </header>
+    // </article>
   );
 };
 
-export default Question
+export default Question;
